9e5a1e8341b2290ee1d5b6d5f1d3f8c2143587e5
Mahmoud.Zakria <mahmoud.zakria@dunnhumby.com>
2019-07-19 15:43:42 +0200

    First love

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <h1>Fancy Camera</h1>
+    <p>Upload files type: .JPEG, .PNG</p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="sass">
+  h1
+    margin-top: 0
+  header
+    background: #efefef
+    padding: 25px
+    text-align: center
+    border-top: 1px solid grey
+    border-bottom: 1px solid grey
+    
+    
+</style>
---
+++ src/components/Uploader.vue
@@ -0,0 +1,27 @@
+<template>
+  <form>
+    <input @change="onUpload" type="file" />
+    <button>Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      file: null
+    };
+  },
+  methods: {
+    onUpload(e) {
+      this.file = e.target.files[0];
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="sass">
+  form
+    margin-top: 60px   
+</style>
