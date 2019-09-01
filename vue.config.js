f4e1b6989b18dc47f8c49a8fcec9c2abd7a2af5a
Mahmoud Zakria <me@zakcoding.com>
2019-09-02 01:08:38 +0200

    updated with master

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
+const path = require("path");
+
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/fancy-camera/dist/" : "/"
+  outputDir: path.resolve(__dirname, "docs"),
+  publicPath: process.env.NODE_ENV === "production" ? "/fancy-camera/" : "/"
 };
