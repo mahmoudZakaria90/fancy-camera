46c44ef797e182984e59fe05545e40de5835ea0b
Mahmoud Zakria <me@zakcoding.com>
2019-12-31 12:09:42 +0100

    added padding to hero-outer, fixed coords for black bg

--- src/components/Dashboard/index.vue
+++ src/components/Dashboard/index.vue
@@ -33,12 +33,15 @@ export default {
       height: 100%
       display: flex
       align-items: center
+      padding: 0 15px
       background: url('../../assets/images/fancy-bg.jpg') no-repeat center center / cover
       &:before
         content: ''
         position: absolute
-        width: 100%
-        height: 100%
+        left: 0
+        right: 0
+        bottom: 0
+        top: 0
         background-color: rgba(0, 0, 0, .6)
     &-inner
       flex-grow: 1
